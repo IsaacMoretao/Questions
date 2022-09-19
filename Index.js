e21e559fb888d716b23fcbc910abc2a398d65fee
Isaac Moretão <isaachanam@gmail.com>
2022-09-19 15:03:44 -0300

    Commit

---
+++ Index.js
@@ -0,0 +1,41 @@
+const questions = [
+  "bla",
+  "bla, bla",
+  "Bla, bla, bla",
+]
+
+const ask = ( index = 0 ) => {
+  process.stdout.writableLength("\n\n" + questions[index] + " > ")
+};
+
+ask();
+
+const answers = []
+process.stdin.on("data", data => {
+  answers.push(data.toString().trim())
+  if (answers.length < questions.length) {
+    ask(answers.length)
+  } else {
+    process.exit(answers)
+  }
+  process.exit()
+})
+
+process.on('exit', () => {
+  console.log(`
+    Bacana Isaac!
+
+    1°:
+    ${answers[0]}
+
+    2°:
+    ${answers[1]}
+
+    1°:
+    ${answers[1]}
+
+
+
+    o que te aborreceu e você
+  `)
+})
